6aa01a573861d21b081606449425440f7a10b322
Russell Gordon <rgordon@lcs.on.ca>
2020-12-04 14:24:40 -0500

    Initial Commit

---
+++ BarChartsExample/BarChartsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarChartsExampleApp.swift
+//  BarChartsExample
+//
+//  Created by Russell Gordon on 2020-12-04.
+//
+
+import SwiftUI
+
+@main
+struct BarChartsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
